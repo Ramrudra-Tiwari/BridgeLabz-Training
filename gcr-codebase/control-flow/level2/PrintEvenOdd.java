cd1d44a8195ed47a9f80104d8a6982f12dedb442
Ramrudra-Tiwari <ramrudhra11@gmail.com>
2025-12-18 17:16:22 +0530

    [RamrudraTiwari] : added code to print even odd numbers

---
+++ gcr-codebase/control-flow/level2/PrintEvenOdd.java
@@ -0,0 +1,26 @@
+//Create a program to print odd and even numbers between 1 to the number entered by the user.
+import java.util.Scanner ;
+public class PrintEvenOdd{
+	public static void main(String[] args){
+		// creating Scanner Object to take user input
+		Scanner Sc = new Scanner(System.in);
+
+
+		// Creating variables to store the value of number given by user
+		int num = Sc.nextInt();
+
+		// Checking if the number is natural or not
+		if(num>0){
+			for(int i = 1 ; i<=num;i++){
+				if(i%2==0){
+					System.out.println("The number "+i+" is an Even number");
+				}
+				else{
+					System.out.println("The number "+i+" is an Odd number");
+
+				}
+			}
+
+		}
+	}
+}	
