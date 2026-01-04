abbf60436bbefef4eef87395a343794284ceab0a
Ramrudra-Tiwari <ramrudhra11@gmail.com>
2026-01-04 17:26:06 +0530

    [RamrudraTiwari] : added code and classes of Vehicle Rental Application

---
+++ java-oops-practice/scenario-based-codebase/ScenarioCodebase/src/com/vehiclerentalapplication/Customer.java
@@ -0,0 +1,18 @@
+package com.vehiclerentalapplication;
+
+//Creating an  Customer class
+public class Customer {
+
+    private String customerId;
+    private String name;
+
+    public Customer(String customerId, String name) {
+        this.customerId = customerId;
+        this.name = name;
+    }
+
+    public void showCustomerDetails() {
+        System.out.println("Customer ID   : " + customerId);
+        System.out.println("Customer Name : " + name);
+    }
+}
---
+++ java-oops-practice/scenario-based-codebase/ScenarioCodebase/src/com/vehiclerentalapplication/Rentable.java
@@ -0,0 +1,6 @@
+package com.vehiclerentalapplication;
+
+// Creating  Interface for rental calculation
+public interface Rentable {
+    double calculateRent(int days);
+}
