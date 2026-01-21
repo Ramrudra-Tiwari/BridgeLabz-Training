31c92842d3bdc0ae814ebede1a9ff12f5726184c
Ramrudra-Tiwari <ramrudhra11@gmail.com>
2026-01-21 11:19:36 +0530

    [RamrudraTiwari] : added code of merge two maps

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/collections/Map/MergeTwoMaps.java
@@ -0,0 +1,32 @@
+package com.collections.Map;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MergeTwoMaps {
+
+    public static void main(String[] args) {
+
+        Map<String, Integer> map1 = new HashMap<>();
+        map1.put("A", 1);
+        map1.put("B", 2);
+
+        Map<String, Integer> map2 = new HashMap<>();
+        map2.put("B", 3);
+        map2.put("C", 4);
+
+        // Result map
+        Map<String, Integer> result = new HashMap<>(map1);
+
+        // Merge map2 into result
+        for (Map.Entry<String, Integer> entry : map2.entrySet()) {
+            String key = entry.getKey();
+            int value = entry.getValue();
+
+            result.put(key, result.getOrDefault(key, 0) + value);
+        }
+
+        System.out.println(result);
+    }
+}
