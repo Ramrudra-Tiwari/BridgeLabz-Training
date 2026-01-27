b04bfa1de83ad559f453c16272d149bddbaf85ed
Ramrudra-Tiwari <ramrudhra11@gmail.com>
2026-01-27 15:37:20 +0530

    [RamrudraTiwari] : added code of finally block execution

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/exceptions/finallyblockexecution/Main.java
@@ -0,0 +1,26 @@
+package com.exceptions.finallyblockexecution;
+
+import java.util.*;
+
+
+public class Main {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String args[]) {
+		try {
+			System.out.println("Enter first number: ");
+			int n1 = sc.nextInt();
+			
+			System.out.println("Enter second number: ");
+			int n2 = sc.nextInt();
+			
+			System.out.println(n1+"/"+n2+"= "+(n1/n2));
+
+		} catch (ArithmeticException e) {
+			System.out.println("can't divide by zero");
+		}
+		finally{
+			System.out.println("Operation completed.");
+		}
+	}
+}
