aa414064b2ed58d2ab37d792f6ac8584ba479097
Ramrudra-Tiwari <ramrudhra11@gmail.com>
2026-01-28 03:48:43 +0530

    [RamrudraTiwari] : added code of invoke private method

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/annotationsreflection/reflection/invokeprivatemethod/Calculator.java
@@ -0,0 +1,9 @@
+
+package com.annotationsreflection.reflection.invokeprivatemethod;
+
+public class Calculator {	
+	@SuppressWarnings("unused")
+	private int mul(int a, int b) {
+		return a*b;
+	}
+}
