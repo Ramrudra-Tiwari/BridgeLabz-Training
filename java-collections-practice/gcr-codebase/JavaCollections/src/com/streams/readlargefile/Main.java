0c91c1258a2e72a317545671b02d542256495ac7
Ramrudra-Tiwari <ramrudhra11@gmail.com>
2026-01-27 12:50:16 +0530

    [RamrudraTiwari] : added code of read large file

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/streams/readlargefile/Main.java
@@ -0,0 +1,27 @@
+package com.streams.readlargefile;
+
+import java.io.*;
+public class Main {
+
+	public static void main(String[] args) {
+		try {
+			BufferedReader read = new BufferedReader(
+					new InputStreamReader(
+							new FileInputStream("textFiles/bigfile.txt")
+						)
+			);
+			
+			String line;
+			while((line=read.readLine())!=null) {
+				if(line.contains("error")) {
+					System.out.println(line);
+				}
+			}
+			
+			read.close();
+		}
+		catch(IOException e) {
+			System.out.println("File not found!");
+		}
+	}
+}
