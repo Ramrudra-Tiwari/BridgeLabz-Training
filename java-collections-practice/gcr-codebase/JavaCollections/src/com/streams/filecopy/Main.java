b2fe743bd4355ed0286766ade01d7b904d096526
Ramrudra-Tiwari <ramrudhra11@gmail.com>
2026-01-27 12:21:39 +0530

    [RamrudraTiwari] : added code of file copy

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/streams/filecopy/Main.java
@@ -0,0 +1,22 @@
+package com.streams.filecopy;
+import java.io.*;
+public class Main {
+
+	public static void main(String[] args) {
+		try(FileInputStream fi = new FileInputStream("textFiles/text.txt")) {
+			System.out.println("File Contents:");
+			
+			FileOutputStream newFile = new FileOutputStream("textFiles/newText.txt");
+			
+            int ch;
+            while ((ch = fi.read()) != -1) {
+                System.out.print((char)ch);
+                newFile.write(ch);
+            }
+			newFile.close();
+		}
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
