cf587de7762be20c1e802747fc5a87bdbdc96f2b
Ramrudra-Tiwari <ramrudhra11@gmail.com>
2026-01-28 03:30:38 +0530

    [RamrudraTiwari] : added code of reflection of access private field

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/annotationsreflection/reflection/accessprivatefield/Person.java
@@ -0,0 +1,11 @@
+package com.annotationsreflection.reflection.accessprivatefield;
+
+public class Person {
+	private String name;
+	private int age;
+	
+	public Person() {
+		this.name = "ravi";
+		this.age = 20;
+	}
+}
