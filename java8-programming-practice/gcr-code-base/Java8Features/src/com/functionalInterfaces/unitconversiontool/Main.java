4eeea7f5df7cc8d91b9ec6beb0e64faad44f9425
Ramrudra-Tiwari <ramrudhra11@gmail.com>
2026-02-01 22:58:00 +0530

    [RamrudraTiwari] : added code of  unit conversion tool

---
+++ java8-programming-practice/gcr-code-base/Java8Features/src/com/functionalInterfaces/unitconversiontool/Main.java
@@ -0,0 +1,14 @@
+
+package com.functionalInterfaces.unitconversiontool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double km = 34.32;
+		double kg = 23;
+		
+		Logistics.kmToMiles(km);
+		Logistics.kgToIbs(kg);
+		
+	}
+}
