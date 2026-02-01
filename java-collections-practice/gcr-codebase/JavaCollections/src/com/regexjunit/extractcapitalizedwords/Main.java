0d7875065f3ed35e037d467415dbd176c6363bf3
Ramrudra-Tiwari <ramrudhra11@gmail.com>
2026-02-01 16:26:29 +0530

    [RamrudraTiwari] : added code of  extra capitalized words

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/regexjunit/extractcapitalizedwords/Main.java
@@ -0,0 +1,19 @@
+package com.regexjunit.extractcapitalizedwords;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String regex = "\\b[A-Z][a-z]+\\b";
+		Pattern pattern = Pattern.compile(regex);
+		
+		String text = "The Eiffel Tower is in Paris and the Statue of Liberty is in New York.";		
+		Matcher matcher = pattern.matcher(text);
+		
+		while(matcher.find()) {
+			System.out.println(matcher.group());
+		}
+	}
+}
