97a5e342121971d630b85dae26c29ca127392eb1
Ramrudra-Tiwari <ramrudhra11@gmail.com>
2026-02-01 16:33:33 +0530

    [RamrudraTiwari] : added code of extract links

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/regexjunit/extractlinks/Main.java
@@ -0,0 +1,20 @@
+package com.regexjunit.extractlinks;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String regex = "https?://(www\\.)?([a-zA-Z0-9.-]+)";
+		
+		Pattern pattern = Pattern.compile(regex);
+		
+		String text = "Visit https://www.google.com and http://example.org for more info.";
+		Matcher matcher = pattern.matcher(text);
+		
+		while(matcher.find()) {
+			System.out.println(matcher.group());
+		}
+	}
+}
