084f61663ba462643067787085428a6438829a9f
Ramrudra-Tiwari <ramrudhra11@gmail.com>
2026-02-01 16:28:27 +0530

    [RamrudraTiwari] : added code of  extra currency values

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/regexjunit/extractcurrencyvalues/Main.java
@@ -0,0 +1,20 @@
+package com.regexjunit.extractcurrencyvalues;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String regex = "\\d+\\.?+\\d+\\b";
+		Pattern pattern = Pattern.compile(regex);
+		
+		String text = "The price is $45.99, and the discount is 10.50. and 20";
+		Matcher matcher = pattern.matcher(text);
+		
+		while(matcher.find()) {
+			System.out.println(matcher.group());
+		}
+	}
+}
