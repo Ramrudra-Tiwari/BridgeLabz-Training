b12402a4eb653a308580ffb9948827da5eb118c1
Ramrudra-Tiwari <ramrudhra11@gmail.com>
2026-02-02 12:23:56 +0530

    [RamrudraTiwari] : Added the progrma file of convert object to json

---
+++ java-iostream-practice/gcr-code-base/JavaIoStreams/src/com/json/convert_object_to_JSON/Main.java
@@ -0,0 +1,21 @@
+package com.json.convert_object_to_JSON;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+        Car car = new Car("Toyota", "Fortuner", 3500000.5);
+
+        
+        ObjectMapper mapper = new ObjectMapper();
+        
+        //convert object to JSON
+        String json = mapper.writeValueAsString(car);
+       
+        System.out.println(json);
+        
+        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(car));
+	}
+}
