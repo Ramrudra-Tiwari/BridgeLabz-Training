d6a710009463645e2588ec94d9c8eb2f50176241
Ramrudra-Tiwari <ramrudhra11@gmail.com>
2026-02-09 12:00:05 +0530

    [RamrudraTiwari] : added code of download manager

---
+++ java-design-pattern-practice/gcr-code-base/JavaDesign/src/com/multithreading/downloadmanager/FileThread.java
@@ -0,0 +1,16 @@
+
+package com.multithreading.downloadmanager;
+
+public class FileThread implements Runnable{
+	
+	FileDownloader filedownloader;
+	
+	public FileThread(FileDownloader f) {
+		this.filedownloader = f;
+	}
+	
+	@Override
+	public void run() {
+		filedownloader.download();
+	}
+}
